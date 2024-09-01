16a35b3ebcd6d00c8d1c2be054d8d3827f71dfbd
olga-kacala <olga.kacala@gmail.com>
2024-09-01 21:35:50 +0200

    lint

--- ByteWars/server/eslint.config.js
+++ ByteWars/server/eslint.config.js
@@ -6,26 +6,15 @@ module.exports = {
       sourceType: 'module',
     },
   },
-  // Extend configurations
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended', // or any other configs you use
-  ],
+  
   // Specify rules
   rules: {
-    'no-console': 'warn',
-    'prettier/prettier': 'error',
-    // Add or modify rules as needed
+
   },
-  // Specify files to ignore
-  ignorePatterns: [
-    'node_modules/**/*',
-    'dist/**/*',
-    'build/**/*',
-  ],
+
   // Plugins can be specified here
   plugins: {
-    prettier: require('eslint-plugin-prettier'),
+  
     node: require('eslint-plugin-node'),
   },
 };
