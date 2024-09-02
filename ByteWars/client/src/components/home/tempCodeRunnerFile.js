f562df376044be9f75dc4d74bda4092e6f1e6ad0
olga-kacala <olga.kacala@gmail.com>
2024-09-02 10:01:47 +0200

    temp removed

--- ByteWars/client/src/components/home/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
- <div className={classes.card} onClick={handleAttack}>
-                <h3>Card</h3>
-                <p>Attack Power:</p>
-                <p>Attack Speed:</p>
-                <p>Luck:</p>
-                
-                
-                </div>
