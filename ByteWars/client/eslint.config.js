d92223e4672bd4ac1b5ba00db3767dcac1c5b672
olga-kacala <olga.kacala@gmail.com>
2024-09-02 10:50:52 +0200

    client side babel config

--- ByteWars/client/eslint.config.js
+++ ByteWars/client/eslint.config.js
@@ -1,14 +1,22 @@
 const globals = require("globals");
 const pluginJs = require("@eslint/js");
+const babelParser = require("@babel/eslint-parser");
 
 module.exports = [
   {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
+  languageOptions: {
+    parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
       },
+    globals: {
+      ...globals.node,
+      ...globals.jest,
     },
   },
+},
   pluginJs.configs.recommended,
 ];
